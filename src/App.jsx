d5fc88663c6b8cc61947c4c993602b810a699b47
Sofiyat1 <sofiyatgbadamosi@gmail.com>
2024-04-21 16:08:24 +0100

    New Pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import SignUp from "./Pages/signupPage/SignUp";
 import NoPage from "./Pages/ErrorPage/NoPage";
 import Contact from "./Pages/ContactPage/Contact"
 import Category from "./Components/category/index"
-import Literacy from "./Pages/DigitalLiteracy/Literacy"
+import Literacy from "./Pages/DigitalLiteracy/Literacy";
+import Aboutus from "./Pages/AboutPage/Aboutus"
+import Resources from "./Pages/ResourcesPage/Resources";
 
 const App = () => {
   return (
@@ -20,8 +22,10 @@ const App = () => {
       <Route path="/login" element={<LoginForm/>}  />
       <Route path="/home" element={<LandingPage/>}  />
       <Route path="/contact" element={<Contact/>} />
+      <Route path="/aboutus" element={<Aboutus />} />
       <Route path="/category" element={<Category/>} />
       <Route path="/literacy" element={<Literacy/>} />
+      <Route path="/resources" element={<Resources/>} />
       <Route path="*" element={<NoPage />} />
      </Routes>
      </BrowserRouter>
